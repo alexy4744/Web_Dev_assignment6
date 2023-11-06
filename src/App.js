63ee8b90cc311b940ee73abad280d3d6c4cbeb99
Shion1314 <jiaxinlin200204@gmail.com>
2023-11-05 23:10:20 -0500

    Using openlibary API

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React, { useState, useEffect } from 'react';
 
 export default function AuthorBooks() {
   const [authorWorks, setAuthorWorks] = useState([]);
+  const [author, setAuthor] = useState(" ");
 
   useEffect(()=>{
   const authorWorksUrl = 'https://openlibrary.org/authors/OL23919A/works.json';
+  const authroUrl = 'https://openlibrary.org/authors/OL23919A.json';
   fetch(authorWorksUrl,{method: "GET"})
   .then((response)=>response.json())
   .then(data=>{
     setAuthorWorks(data.entries)
   })
+  fetch(authroUrl,{method: "GET"})
+  .then((response)=>response.json())
+  .then(data=>{
+    setAuthor(data.personal_name)
+  })
   });
   return (
     <div>
-      <h1>Someone's Books</h1>
+      <h1>{author}'s Books</h1>
       <div className="table-container">
         <table>
           <thead>
